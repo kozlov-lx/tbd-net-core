2bde0a7e77f61ff77881389f4bb49c789634c0ad
Alexey A. Kozlov <aa.kozlov@i-sys.ru>
2020-07-31 16:54:05 +0400

    init solution

---
+++ web/Program.cs
@@ -0,0 +1,19 @@
+namespace tbd.web
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
+    }
+}
